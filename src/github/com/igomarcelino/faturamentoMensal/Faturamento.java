bf4384d4d53fb7c4dc6625629c863636f99c2c7e
Igo Marcelino <geslucian@hotmail.com>
2024-09-30 13:15:11 -0300

    Ajustando o formato do arquivo json para o formato de apoio para o desafio, adiciona o metodo deserialize XML para realizar leitura a partir do formato de arquivo de apoio, adiciona uma verificacao de extensao de arquivo

--- src/github/com/igomarcelino/faturamentoMensal/Faturamento.java
+++ src/github/com/igomarcelino/faturamentoMensal/Faturamento.java
@@ -3,10 +3,17 @@ package github.com.igomarcelino.faturamentoMensal;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import javax.swing.*;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -23,9 +30,15 @@ import java.util.List;
 public class Faturamento {
     public static void main(String[] args) {
         List<FaturamentoDiario> faturamentoDiarios = new ArrayList<>();
-        System.out.println("Informe o local do arquivo json");
+        System.out.println("Informe o local do arquivo");
         String caminhoArquivo = caminhoArquivo();
-        faturamentoDiarios = faturamentoDiarios(caminhoArquivo);
+        // verifica a extensao do arquivo pelo final do caminho do arquivo
+        if (caminhoArquivo.endsWith(".json")){
+            faturamentoDiarios = deserializeJson(caminhoArquivo);
+        }else if (caminhoArquivo.endsWith(".xml")){
+            faturamentoDiarios = deserializeXML(caminhoArquivo);
+        }
+
         if (!faturamentoDiarios.isEmpty()) {
             System.out.println("Dia com menor Faturamento");
             diaComMenorFaturamento(faturamentoDiarios);
@@ -36,13 +49,24 @@ public class Faturamento {
             System.out.println("Dias com faturamento acima da media mensal");
             diaFaturamentoSuperiorMediaMensal(faturamentoDiarios);
         } else {
-            System.out.println("Finalizando ....");
+            System.out.println("Nao possui faturamento, finalizando ....");
         }
     }
 
-    // esse metodo retorna uma Lista<Faturamento> se o arquivo passado no caminho for correspondente ao modelo esperado
+    /**
+     * metodo que obtem o caminho do arquivo informado
+     * */
 
-    private static List<FaturamentoDiario> faturamentoDiarios(String caminho) {
+    public static String caminhoArquivo() {
+        // Utiliza o JFileChooser para localizar e selecionar o arquivo no sistema
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.showOpenDialog(null);
+        return fileChooser.getSelectedFile().getAbsolutePath();
+    }
+    /**
+     * esse metodo realiza um deserialize do json para uma Lista<FaturamentoDiario>
+     */
+    private static List<FaturamentoDiario> deserializeJson(String caminho) {
         // Cria um arrayList para armazenar os faturamentos diarios
         List<FaturamentoDiario> faturamentoDiariosList = new ArrayList<>();
         //Cria uma instancia da classe gson
@@ -65,42 +89,117 @@ public class Faturamento {
         return faturamentoDiariosList;
     }
 
-    public static String caminhoArquivo() {
-        // Utiliza o JFileChooser para localizar e selecionar o arquivo no sistema
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.showOpenDialog(null);
-        return fileChooser.getSelectedFile().getAbsolutePath();
+/**
+ * esse metodo realiza um deserialize do xml para uma Lista<FaturamentoDiario>
+ * */
+    private static List<FaturamentoDiario> deserializeXML(String caminho){
+        // Cria uma lista do farutamento diario
+        List<FaturamentoDiario> faturamentoDiarioList = new ArrayList<>();
+        try {
+
+            // Cria um leitor de arquivo a partir de um caminho
+            FileReader fileReader = new FileReader(caminho);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            StringBuilder arquivoXml = new StringBuilder();
+            String linha;
+
+            // verifica se existe linha ser lida
+            while ((linha = bufferedReader.readLine())!=null){
+                // adiciona a linha linha lida no stringbuilder
+                arquivoXml.append(linha);
+            }
+            // fecha o bufferedReader
+            bufferedReader.close();
+
+            // Para que o DOM funcione o arquivo xml precisa ter a estrutura correta, entao adicionaremos a tag
+            // <rows></rows> para que o dom possa ler cada row dentro de rows
+            String conteudoXML = "<rows>"+arquivoXml.toString()+"</rows>";
+
+            /**
+             * Poderiamos apagar da linha 101 ate a linha 134 caso o formato do arquivo xml contensse a tag raiz, por exemplo
+             * <rows> // tag raiz
+             *     <row>
+             *         <dia></dia>
+             *         <valor></valor>
+             *     </row>
+             * </rows>
+             *
+             * dessa forma poderiamos utilizar
+             * DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+             * DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+             * Document document = documentBuilder.parse("caminho do arquivo xml contendo a tag root");
+             * */
+            // Cria uma nova instancia do documentbuilder factory, permitindo criar um parser a partir de um modelo de documento de objeto
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+
+            // Ao que entendi, esse builder utiliza o builderfactory para criar um parser , a onde consiguira ler as
+            // o conteudo das tags
+            DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
+            Document document = builder.parse(new InputSource(new StringReader(conteudoXML)));
+
+            // Cria uma lista de nos de todas as tags row contidas no documento
+            NodeList nodeList = document.getElementsByTagName("row");
+
+            // Percorre o nodelist para transformar cado no em um elemento com a ajuda do Element
+                for (int i = 0; i < nodeList.getLength();i++){
+                    // cria um elemento ( aqui cada row se tornara um elemento para inserirmos em nossa lista )
+                    Element row = (Element) nodeList.item(i);
+                    FaturamentoDiario faturamentoDiario = new FaturamentoDiario();
+                    // captura o conteudo das tags dia e valor
+                    String dia = row.getElementsByTagName("dia").item(0).getTextContent();
+                    String valor = row.getElementsByTagName("valor").item(0).getTextContent();
+
+                    // atribui os conteudos das tags dias e valor para o nosso objeto faturamentoDiario
+                    faturamentoDiario.setDia(dia);
+                    faturamentoDiario.setValor(Double.valueOf(valor));
+
+                    // adiciona o faturamentoDiario a lista
+                    faturamentoDiarioList.add(faturamentoDiario);
+                }
+        } catch (ParserConfigurationException e) {
+            System.out.println("Nao foi possivel converter o arquivo");
+        } catch (IOException e) {
+            System.out.println("Verificar arquivo informado");
+        } catch (SAXException e) {
+            // nesse ponto eu precisaria aprender uma forma de como tratar essa exception
+            System.out.println("Estrutura do arquivo invalido");
+        }
+
+        return faturamentoDiarioList;
     }
 
-    // metodo que percorre a lista e imprime o dia com menor faturamento
+    /**
+     * metodo que percorre a lista e imprime o dia com menor faturamento
+     * */
     public static void diaComMenorFaturamento(List<FaturamentoDiario> faturamentoDiarioList) {
-        faturamentoDiarioList.stream().
-                sorted((o1, o2) -> Double.valueOf(o1.getFaturamento()).compareTo(o2.getFaturamento())).
+        faturamentoDiarioList.stream().filter(valorZero -> valorZero.getValor() != 0.0).
+                sorted((o1, o2) -> Double.valueOf(o1.getValor()).compareTo(o2.getValor())).
                 limit(1).
-                forEach(faturamentoDiario -> System.out.println("Dia: " + faturamentoDiario.getDia_mes() + "" +
-                        "                       \nFaturamento diario: " + faturamentoDiario.getFaturamento()));
+                forEach(FaturamentoDiario::imprimirFaturamento);
     }
 
-    // metodo que percorre a lista e imprime o dia com maior faturamento
+    /**
+     * metodo que percorre a lista e imprime o dia com maior faturamento
+     * */
     public static void diaComMaiorFaturamento(List<FaturamentoDiario> faturamentoDiarioList) {
-        faturamentoDiarioList.stream().
-                sorted((o1, o2) -> Double.valueOf(o1.getFaturamento()).compareTo(o2.getFaturamento())).
+        faturamentoDiarioList.stream().filter(valorZero -> valorZero.getValor() != 0.0).
+                sorted((o1, o2) -> Double.valueOf(o1.getValor()).compareTo(o2.getValor())).
                 sorted(Collections.reverseOrder()).
                 limit(1).
-                forEach(faturamentoDiario -> System.out.println("Dia: " + faturamentoDiario.getDia_mes() + "" +
-                        "                       \nFaturamento diario: " + faturamentoDiario.getFaturamento()));
+                forEach(FaturamentoDiario::imprimirFaturamento);
     }
 
-    // metodo que percorre a lsita e imprime todos os dias que tiveram o faturamento maior que a media mensal
+    /**
+     * metodo que percorre a lsita e imprime todos os dias que tiveram o faturamento maior que a media mensal
+     * */
     public static void diaFaturamentoSuperiorMediaMensal(List<FaturamentoDiario> faturamentoDiarios){
        double mediaMensal =  faturamentoDiarios.stream().
-                filter(faturamentoDiario -> faturamentoDiario.getFaturamento() != 0).
-                mapToDouble(valorDiario-> valorDiario.getFaturamento()).average().getAsDouble();
-        System.out.println(mediaMensal);
+                filter(faturamentoDiario -> faturamentoDiario.getValor() != 0).
+                mapToDouble(valorDiario-> valorDiario.getValor()).average().getAsDouble();
        faturamentoDiarios.stream().
-               filter(faturamentoDiario -> faturamentoDiario.getFaturamento() > mediaMensal).
-               forEach(faturamentoDiario -> System.out.println("Dia: " + faturamentoDiario.getDia_mes() + "" +
-                       "                       \nFaturamento diario: " + faturamentoDiario.getFaturamento()));
+               filter(faturamentoDiario -> faturamentoDiario.getValor() > mediaMensal).
+               forEach(FaturamentoDiario::imprimirFaturamento);
     }
 }
 
--- src/github/com/igomarcelino/faturamentoMensal/FaturamentoDiario.java
+++ src/github/com/igomarcelino/faturamentoMensal/FaturamentoDiario.java
@@ -1,45 +1,41 @@
 package github.com.igomarcelino.faturamentoMensal;
 
 public class FaturamentoDiario implements Comparable<FaturamentoDiario> {
-    private int dia_mes;
-    private String dia_semana;
-    private double faturamento;
+    private String dia;
 
-    public int getDia_mes() {
-        return dia_mes;
+    private double valor;
+
+    public String getDia() {
+        return dia;
     }
 
-    public void setDia_mes(int dia_mes) {
-        this.dia_mes = dia_mes;
+    public void setDia(String dia) {
+        this.dia = dia;
     }
 
-    public String getDia_semana() {
-        return dia_semana;
+
+    public double getValor() {
+        return valor;
     }
 
-    public void setDia_semana(String dia_semana) {
-        this.dia_semana = dia_semana;
-    }
-
-    public double getFaturamento() {
-        return faturamento;
-    }
-
-    public void setFaturamento(double faturamento) {
-        this.faturamento = faturamento;
+    public void setValor(double valor) {
+        this.valor = valor;
     }
 
     @Override
     public String toString() {
         return "FaturamentoDiario{" +
-                "dia_mes=" + dia_mes +
-                ", dia_semana='" + dia_semana + '\'' +
-                ", faturamento=" + faturamento +
+                "dia=" + dia +
+                ", valor=" + valor +
                 '}';
+    }
+
+    public  void imprimirFaturamento(){
+        System.out.printf("Dia: %s\nValor: %.2f\n",dia,valor);
     }
 
     @Override
     public int compareTo(FaturamentoDiario o) {
-        return Double.compare(faturamento,o.getFaturamento());
+        return Double.compare(valor,o.getValor());
     }
 }
